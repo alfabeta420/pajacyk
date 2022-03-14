30c99435e45e19596eaaac5f341bfad8afce62e7
bremu45 <81532283+bremu45@users.noreply.github.com>
2022-03-14 21:57:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ a = input()
 a = int(a)
 b = 0
 while b < a:
-    r = requests.post("https://www.pajacyk.pl/api/clicks")
+    requests.post("https://www.pajacyk.pl/api/clicks")
     print(f"Wysłano kliknięcie nr {b}")
     b = b + 1
 print("Zakonczono klikanie w przycisk :) milego dnia")
